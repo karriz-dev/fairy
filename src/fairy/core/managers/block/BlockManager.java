6e730264dd23fa2e2fdc62d57b8d977ee64a8b76
ldh1428a <ldh1428a@naver.com>
2019-01-21 11:48:16 +0900

    fairy_commit_2

--- src/fairy/core/managers/block/BlockManager.java
+++ src/fairy/core/managers/block/BlockManager.java
@@ -1,27 +1,63 @@
 package fairy.core.managers.block;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import fairy.core.utils.Debugger;
+import fairy.valueobject.managers.block.Block;
+import fairy.valueobject.managers.transaction.TokenTransaction;
+import fairy.valueobject.managers.transaction.Transaction;
 
-public class BlockManager {
+public class BlockManager extends Thread {
+	
+	public final String genesisTokenAddress = "";
+	public final Double genesisTokenAmount = 10081008.0;
+	
+	private Map<String, Double> genesisTokenMap = null;
+	
+	private static BlockManager instance = null;
+	
 	private BlockManager() {
+		genesisTokenMap = new HashMap<String, Double>();
+		if(generatorGenesis())
+		{
+			this.start();
+		}
+	}
+	
+	@Override
+	public void run()
+	{
 		
 	}
 	
-	public boolean generatorGenesis()
+	private boolean generatorGenesis()
 	{
-		try {
-			
-			File file = new File("assets/block/genesis.fairyblock");
-			if(!file.exists())
-			{
-				file.createNewFile();
-				return true;
-			}else return false;
-		}catch(Exception e) {
-			Debugger.Log(this, e);
-			return false;
+		String blockid = "";
+		
+		Long timestamp = 1548065280L;
+				
+		genesisTokenMap.put("0x00", 10081008.0);
+		
+		List<Transaction> txlist = new ArrayList<Transaction>();
+
+		Transaction tx = new TokenTransaction(genesisTokenMap);
+		
+		txlist.add(tx);
+		
+		String merkleroot = MerkleTree.getMerkleRoot(txlist);
+				
+		Block block = new Block(blockid, timestamp, merkleroot, txlist);
+		
+		return block.saveBlock();
+	}
+	
+	public static BlockManager getInstance()
+	{
+		if(instance==null) {
+			instance = new BlockManager();
 		}
+		return instance;
 	}
 }
---
+++ src/fairy/core/managers/block/MerkleTree.java
@@ -0,0 +1,11 @@
+package fairy.core.managers.block;
+
+import java.util.List;
+
+import fairy.valueobject.managers.transaction.Transaction;
+
+public class MerkleTree {
+	public static String getMerkleRoot(List<Transaction> txlist) {
+		return "Root";
+	}
+}
