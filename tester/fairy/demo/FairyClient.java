399e66c7f45c0a31d8068d417effc462b9d24644
ldh1428a <ldh1428a@naver.com>
2019-01-23 17:38:32 +0900

    environment_commit

--- tester/fairy/demo/FairyClient.java
+++ tester/fairy/demo/FairyClient.java
@@ -1,13 +1,16 @@
 package fairy.demo;
 
 import fairy.core.command.CommandLayout;
-import fairy.core.managers.transaction.TransactionManager;
+import fairy.core.command.CommandList;
 import fairy.core.net.communicator.Linker;
 import fairy.core.net.communicator.Session;
 
 public class FairyClient {
-	public static void main(String[] args) {	
-		Session.getInstance();
+	public static void main(String[] args) {
+		
+		CommandList.test();
+		
+		/*Session.getInstance();
 		
 		// waiting session is opened
 		while(Session.getInstance().isOpened());
@@ -16,6 +19,6 @@ public class FairyClient {
 		Linker.getInstance();
 		
 		//CommandLineInterface Layout Call
-		CommandLayout.getInstance();
+		CommandLayout.getInstance();*/
 	}
 }
