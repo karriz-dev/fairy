3b47de65262de612b9f3dff373543012f9dc2541
ldh1428a <ldh1428a@naver.com>
2019-01-23 18:14:27 +0900

    environment_commit_2

--- tester/fairy/demo/FairyClient.java
+++ tester/fairy/demo/FairyClient.java
@@ -1,16 +1,18 @@
 package fairy.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import fairy.core.command.CommandLayout;
-import fairy.core.command.CommandList;
+import fairy.core.managers.key.KeyManager;
 import fairy.core.net.communicator.Linker;
 import fairy.core.net.communicator.Session;
+import fairy.valueobject.managers.transaction.TokenTransaction;
+import fairy.valueobject.managers.transaction.Transaction;
 
 public class FairyClient {
 	public static void main(String[] args) {
-		
-		CommandList.test();
-		
-		/*Session.getInstance();
+		Session.getInstance();
 		
 		// waiting session is opened
 		while(Session.getInstance().isOpened());
@@ -19,6 +21,18 @@ public class FairyClient {
 		Linker.getInstance();
 		
 		//CommandLineInterface Layout Call
-		CommandLayout.getInstance();*/
+		CommandLayout.getInstance();
+		
+		KeyManager.getInstance().Create(100);
+		
+		String ftxid = "0x000000000000000000000000000000";
+		String ftxaddress = "0x000000000000000000000000000000";
+		Map<String, Double> output = new HashMap<String ,Double>();
+		output.put("", 10081008.0);
+		
+		Transaction tx = new TokenTransaction(ftxid, ftxaddress, output);
+		
+		KeyManager.getInstance().Sign(tx.getBytes(),"");
+		KeyManager.getInstance().Verify(tx.getSignature(),"");
 	}
 }
