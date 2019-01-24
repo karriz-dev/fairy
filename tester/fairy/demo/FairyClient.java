0bab2d26410cdb116c519affc5431baab3819748
ldh1428a <ldh1428a@naver.com>
2019-01-24 18:49:20 +0900

    environment_commit_3

--- tester/fairy/demo/FairyClient.java
+++ tester/fairy/demo/FairyClient.java
@@ -1,18 +1,16 @@
 package fairy.demo;
 
+import java.security.KeyPair;
 import java.util.HashMap;
 import java.util.Map;
 
-import fairy.core.command.CommandLayout;
 import fairy.core.managers.key.KeyManager;
-import fairy.core.net.communicator.Linker;
-import fairy.core.net.communicator.Session;
 import fairy.valueobject.managers.transaction.TokenTransaction;
 import fairy.valueobject.managers.transaction.Transaction;
 
 public class FairyClient {
 	public static void main(String[] args) {
-		Session.getInstance();
+		/*Session.getInstance();
 		
 		// waiting session is opened
 		while(Session.getInstance().isOpened());
@@ -21,18 +19,24 @@ public class FairyClient {
 		Linker.getInstance();
 		
 		//CommandLineInterface Layout Call
-		CommandLayout.getInstance();
+		CommandLayout.getInstance();*/
 		
 		KeyManager.getInstance().Create(100);
 		
-		String ftxid = "0x000000000000000000000000000000";
-		String ftxaddress = "0x000000000000000000000000000000";
+		String ftxid = "12345678123456781234567812345678";
+		String ftxaddress = "12345678123456781234567812345678";
 		Map<String, Double> output = new HashMap<String ,Double>();
 		output.put("", 10081008.0);
 		
 		Transaction tx = new TokenTransaction(ftxid, ftxaddress, output);
 		
-		KeyManager.getInstance().Sign(tx.getBytes(),"");
-		KeyManager.getInstance().Verify(tx.getSignature(),"");
+		KeyPair pair = KeyManager.getInstance().getKeyPair();
+		
+		tx.setSignature(KeyManager.getInstance().Sign(tx.getBytes(),pair.getPrivate()));
+		
+		if(KeyManager.getInstance().Verify(tx, pair.getPublic()))
+		{
+			System.out.println("OK");
+		}
 	}
 }
