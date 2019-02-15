e338e035b2f8193c74beb279abebb658c28b14cc
ldh1428a <ldh1428a@naver.com>
2019-02-15 12:15:20 +0900

    block_commit

--- src/fairy/valueobject/managers/block/Block.java
+++ src/fairy/valueobject/managers/block/Block.java
@@ -1,25 +1,33 @@
 package fairy.valueobject.managers.block;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Queue;
 
 import fairy.core.security.MerkleTree;
+import fairy.core.utils.Debugger;
+import fairy.valueobject.managers.transaction.TokenTransaction;
 import fairy.valueobject.managers.transaction.Transaction;
 
 public class Block {
 	//Header
-	private String blockid = null;
-	private String prevblockid = null;
+	private String bid = null;
+	private String prevbid = null;
 	private Long timestamp = 0L;
 	private String merkleroot = null;
+	
+	private Long height = 0L;
 	
 	private String creator = null;
 	
 	//Body
 	private Queue<Transaction> txqueue = null;
 	
+	//File 
+	private File blockFile = null;
+	
 	public Block(String blockPath){
-		
+		blockFile = new File(blockPath);
 	}
 
 	public Block(String creator, Queue<Transaction> txqueue) {
@@ -29,15 +37,40 @@ public class Block {
 		this.txqueue = txqueue;
 	}
 
-	public boolean saveBlock() {
-		return false;
-	}
-
-	public boolean loadBlock() {
-		return false;
+	public boolean Create() {
+		try {
+			File file = new File(bid + ".block");
+			if(!file.exists())
+			{
+				// block header 저장 
+				
+				// block body 저장
+			}else return false;
+			return true;
+		}catch(Exception e) {
+			Debugger.Log(this, e);
+			return false;
+		}
 	}
 	
-	public double getFees() {
-		return 0.0f;
+	public double getBalance() {
+		double balance = 0.0;
+		for(Transaction tx: txqueue)
+		{
+			TokenTransaction token = (TokenTransaction)tx;
+			
+			balance += token.getBalance();
+		}
+		return balance;
+	}
+	
+	public byte[] getBytes() {
+		try {
+			FileInputStream fis = new FileInputStream(blockFile);
+			return fis.readAllBytes();
+		}catch(Exception e) {
+			return null;
+		}
+		
 	}
 }
