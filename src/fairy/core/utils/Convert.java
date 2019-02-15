09771e57281d7cdc9fe132354b156ce3b7d56fca
ldh1428a <ldh1428a@naver.com>
2019-02-15 12:15:59 +0900

    convert_commit

--- src/fairy/core/utils/Convert.java
+++ src/fairy/core/utils/Convert.java
@@ -3,6 +3,8 @@ package fairy.core.utils;
 import java.nio.ByteBuffer;
 
 public class Convert {
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
 	public static byte[] ShortToByteArray(short s) {
 		return new byte[]{(byte)((s >> 8) & 0xff), (byte)(s & 0xff)};
 	}
@@ -35,4 +37,14 @@ public class Convert {
 				(((int)bytes[2] & 0xff) << 8) |
 				(((int)bytes[3] & 0xff)));
 	} 
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
 }
