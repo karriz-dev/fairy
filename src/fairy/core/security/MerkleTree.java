33cfc1bd4dc7f0e82786575dea3ebf4d0da4badc
ldh1428a <ldh1428a@naver.com>
2019-03-24 11:06:26 +0900

    merkletree_commit

--- src/fairy/core/security/MerkleTree.java
+++ src/fairy/core/security/MerkleTree.java
@@ -8,11 +8,11 @@ public class MerkleTree {
 	
 	public static String getMerkleRoot(List<Transaction> txlist) {
 		
-		String root = Shield.SHA256(txlist.get(0).getBytes());
+		String root = Shield.SHA256(txlist.get(0).toString().getBytes());
 	
 		for(int i=0;i<txlist.size()-1;i++)
 		{
-			root = Shield.SHA256(root + txlist.get(i+1).getBytes());
+			root = Shield.SHA256(root + txlist.get(i+1).toString().getBytes());
 		}
 		
 		return root;
