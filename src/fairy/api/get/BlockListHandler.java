a536703ac4b60ed7977c44319f03b0e6717cae96
ldh1428a <ldh1428a@naver.com>
2019-03-24 14:21:29 +0900

    get_post_requset_code_modify_commit

--- src/fairy/api/get/BlockListHandler.java
+++ src/fairy/api/get/BlockListHandler.java
@@ -11,29 +11,35 @@ import fairy.api.Handler;
 import fairy.core.managers.ledger.LedgerManager;
 
 public class BlockListHandler extends Handler implements HttpHandler {
-
+	
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		if(exchange.getRequestMethod().toUpperCase().equals(Handler.GET))
+		{
+			Gson gson = new Gson();
 		
-		Gson gson = new Gson();
-		
-		String response = gson.toJson(LedgerManager.getInstance().getLatestBlock());
-		
-		System.out.println(response);
-		
-	    OutputStream os = exchange.getResponseBody();
-	    
-	    if(response != "")
-	    {
-	    	exchange.sendResponseHeaders(200, response.length());
-	    	os.write(response.toString().getBytes());
-	    }else
-	    {
-	    	response = "block is not load wating for create block..";
-	    	exchange.sendResponseHeaders(200, response.length());
-	    	os.write(response.toString().getBytes());
-	    }
-	    
-	    os.close();
+			String response = gson.toJson(LedgerManager.getInstance().getLatestBlock());
+			
+			System.out.println(response);
+			
+		    OutputStream os = exchange.getResponseBody();
+		    
+		    if(response != "")
+		    {
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }else
+		    {
+		    	response = "block is not load wating for create block..";
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }
+		    
+		    os.close();
+		}
+		else {
+			exchange.sendResponseHeaders(400, 0);
+			exchange.close();
+		}
 	}
 }
--- src/fairy/api/post/TokenTransactionHandler.java
+++ src/fairy/api/post/TokenTransactionHandler.java
@@ -21,69 +21,75 @@ import fairy.valueobject.managers.transaction.Transaction;
 public class TokenTransactionHandler extends Handler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		
-		Map<String, Object> parameters = new HashMap<String, Object>();
-        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
-        BufferedReader br = new BufferedReader(isr);
-        String query = br.readLine();
-        parseQuery(query, parameters);
-        
-        String response = "";
+		if(exchange.getRequestMethod().toUpperCase().equals(Handler.POST))
+		{
+			Map<String, Object> parameters = new HashMap<String, Object>();
+	        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+	        BufferedReader br = new BufferedReader(isr);
+	        String query = br.readLine();
+	        parseQuery(query, parameters);
+	        
+	        String response = "";
 
-        if(!parameters.isEmpty())
-        {
-        	Map<String, Double> outputList = new HashMap<String, Double>();
-            
-        	String ftxid = "";
-        	String ftxaddress = "";
-        	
-            for (String key : parameters.keySet()) {
-            	switch(key){
-            	case "ftxid":
-            		ftxid = (String)parameters.get(key);
-            		break;
-            	case "ftxaddress":
-            		ftxaddress = (String)parameters.get(key);
-            		break;
-            	default:
-            		outputList.put(key, Double.valueOf((String)parameters.get(key)));
-            		break;
-            	}
-            }
-            
-            
-            System.out.println("============================= API HANDLER WORKED !! =============================");
-            System.out.println("FTXID: " + ftxid);
-            System.out.println("FTXADDRESS: " + ftxaddress);            
-            System.out.println("OUTPUT LIST: " + outputList);
-            System.out.println("============================= API HANDLER WORKED !! =============================");
-            
-            Transaction tx = new TokenTransaction(ftxid, ftxaddress, outputList);
-            
-            KeyPair pair = KeyManager.getInstance().getKeyPair();
-            
-            tx.setSignature(TransactionManager.getInstance().Sign(tx.getBytes(), pair.getPrivate()));
-            
-            response = tx.toString();
-            
-            Session.getInstance().broadcastingTransaction(tx);
-            
-    		exchange.sendResponseHeaders(200, response.length());
-        }
-        else
-        {
-        	exchange.sendResponseHeaders(500, response.length());
-        
-        	response = "transaction not send... check the arguments !!";
-        }
-        
-        OutputStream os = exchange.getResponseBody();
-        
-        if(response != "")
-        {
-        	 os.write(response.toString().getBytes());
-        }
-        
-        os.close();
+	        if(!parameters.isEmpty())
+	        {
+	        	Map<String, Double> outputList = new HashMap<String, Double>();
+	            
+	        	String ftxid = "";
+	        	String ftxaddress = "";
+	        	
+	            for (String key : parameters.keySet()) {
+	            	switch(key){
+	            	case "ftxid":
+	            		ftxid = (String)parameters.get(key);
+	            		break;
+	            	case "ftxaddress":
+	            		ftxaddress = (String)parameters.get(key);
+	            		break;
+	            	default:
+	            		outputList.put(key, Double.valueOf((String)parameters.get(key)));
+	            		break;
+	            	}
+	            }
+	            
+	            
+	            System.out.println("============================= API HANDLER WORKED !! =============================");
+	            System.out.println("FTXID: " + ftxid);
+	            System.out.println("FTXADDRESS: " + ftxaddress);            
+	            System.out.println("OUTPUT LIST: " + outputList);
+	            System.out.println("============================= API HANDLER WORKED !! =============================");
+	            
+	            Transaction tx = new TokenTransaction(ftxid, ftxaddress, outputList);
+	            
+	            KeyPair pair = KeyManager.getInstance().getKeyPair();
+	            
+	            tx.setSignature(TransactionManager.getInstance().Sign(tx.getBytes(), pair.getPrivate()));
+	            
+	            response = tx.toString();
+	            
+	            Session.getInstance().broadcastingTransaction(tx);
+	            
+	    		exchange.sendResponseHeaders(200, response.length());
+	        }
+	        else
+	        {
+	        	exchange.sendResponseHeaders(500, response.length());
+	        
+	        	response = "transaction not send... check the arguments !!";
+	        }
+	        
+	        OutputStream os = exchange.getResponseBody();
+	        
+	        if(response != "")
+	        {
+	        	 os.write(response.toString().getBytes());
+	        }
+	        
+	        os.close();
+		}
+		else {
+			exchange.sendResponseHeaders(400, 0);
+			exchange.close();
+		}
 	}
 }
