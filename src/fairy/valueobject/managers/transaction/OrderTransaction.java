98f252de1b2b926634098e936de2bc390ffa67dc
YUJEONG <YUJEONG@DESKTOP-GSA43S8>
2019-03-27 15:12:51 +0900

    make OrderTransaction class

--- src/fairy/valueobject/managers/transaction/OrderTransaction.java
+++ src/fairy/valueobject/managers/transaction/OrderTransaction.java
@@ -1,5 +1,24 @@
 package fairy.valueobject.managers.transaction;
 
-public class testTransaction {
+public class OrderTransaction extends Transaction{
 
+	
+	private String buyerAddress = null;
+	private short hydrogenType = 0;
+	private long maxTime = 0;
+	
+	public OrderTransaction() {
+		super(TransactionType.ORDER);
+	}
+	
+	public OrderTransaction(byte[][] stream) {
+		super(stream);
+	}
+
+	@Override
+	protected byte[] getDatasBytes() {
+		
+		return null;
+	}
+	
 }
