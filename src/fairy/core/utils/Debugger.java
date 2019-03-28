e92a7c705cf57802b13d148029f82b46155dafc0
ldh1428a <ldh1428a@naver.com>
2019-03-28 09:57:37 +0900

    debug_level_add_commit

--- src/fairy/core/utils/Debugger.java
+++ src/fairy/core/utils/Debugger.java
@@ -2,11 +2,22 @@ package fairy.core.utils;
 
 public class Debugger {
 	public static boolean isDebugging = true;
+	public static final int level = 0;
 	
 	public static void Log(Object o, String msg)
 	{	
 		if(isDebugging) {
 			System.out.println("[DEBUG] " + o.getClass().getName() +": " + msg);
+		}
+	}
+	
+	public static void Log(Object o, String msg, int lv)
+	{	
+		if(level == lv)
+		{
+			if(isDebugging) {
+				System.out.println("[DEBUG] " + o.getClass().getName() +": " + msg);
+			}
 		}
 	}
 	
