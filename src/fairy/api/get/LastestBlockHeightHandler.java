809746724b8aae474fec63c8c7002bf9070398a1
ldh1428a <ldh1428a@naver.com>
2019-03-28 11:01:37 +0900

    lastest_block_height_handler_add_commit

---
+++ src/fairy/api/get/LastestBlockHeightHandler.java
@@ -0,0 +1,44 @@
+package fairy.api.get;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import fairy.api.Handler;
+import fairy.core.managers.ledger.LedgerManager;
+
+public class LastestBlockHeightHandler extends Handler implements HttpHandler {
+
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		if(exchange.getRequestMethod().toUpperCase().equals(Handler.GET))
+		{
+			Gson gson = new Gson();
+		
+			Long height = LedgerManager.getInstance().getLatestBlock().getHeight();
+			String response = gson.toJson(height);
+			
+		    OutputStream os = exchange.getResponseBody();
+		    
+		    if(response != "")
+		    {
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }else
+		    {
+		    	response = "block is not load wating for create block..";
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }
+		    
+		    os.close();
+		}
+		else {
+			exchange.sendResponseHeaders(400, 0);
+			exchange.close();
+		}
+	}
+}
