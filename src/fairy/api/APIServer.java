4fa4b41a79ca85aacbe2766505c4c1951bf0ef08
ldh1428a <ldh1428a@naver.com>
2019-03-28 11:00:59 +0900

    add_api_lastest_height_commmit

--- src/fairy/api/APIServer.java
+++ src/fairy/api/APIServer.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 import fairy.api.get.BlockListHandler;
+import fairy.api.get.LastestBlockHeightHandler;
 import fairy.api.post.TokenTransactionHandler;
 import fairy.core.utils.Debugger;
 
@@ -19,11 +20,14 @@ public class APIServer {
 		try {
 			httpServer = HttpServer.create(new InetSocketAddress(12075), 0);
 			
+			//transaction
 			httpServer.createContext("/transaction/token",new TokenTransactionHandler());
-			
 			httpServer.createContext("/transaction/list",new BlockListHandler());
 			
+			//block
 			httpServer.createContext("/block/list",new BlockListHandler());
+			httpServer.createContext("/block/lastest/height",new LastestBlockHeightHandler());
+			
 			
 			httpServer.setExecutor(null);
 			httpServer.start();
