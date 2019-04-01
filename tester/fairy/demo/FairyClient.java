fc10bdacf9ec3efdb7dd9aec8c18e67b344fe873
ldh1428a <ldh1428a@naver.com>
2019-04-01 14:29:53 +0900

    rebase_commit

--- tester/fairy/demo/FairyClient.java
+++ tester/fairy/demo/FairyClient.java
@@ -1,18 +1,15 @@
 package fairy.demo;
 
 import fairy.api.APIServer;
-import fairy.core.command.CommandLayout;
 import fairy.core.managers.block.BlockManager;
 import fairy.core.managers.key.KeyManager;
 import fairy.core.managers.ledger.LedgerManager;
 import fairy.core.managers.transaction.TransactionManager;
 import fairy.core.net.communicator.Linker;
 import fairy.core.net.communicator.Session;
-import fairy.core.utils.Network;
 
 public class FairyClient {
 	public static void main(String[] args) {
-		
 		// KeyManager Call
 		KeyManager.getInstance("assets/wallet/wallet.fairy");
 		
