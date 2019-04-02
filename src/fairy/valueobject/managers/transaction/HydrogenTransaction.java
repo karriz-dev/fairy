41adb79f4158a0389b0902e63f1b3aebc76e9df0
ldh1428a <ldh1428a@naver.com>
2019-04-02 20:38:40 +0900

    hydrogentransaction_commit

--- src/fairy/valueobject/managers/transaction/HydrogenTransaction.java
+++ src/fairy/valueobject/managers/transaction/HydrogenTransaction.java
@@ -2,17 +2,22 @@ package fairy.valueobject.managers.transaction;
 
 public class HydrogenTransaction extends Transaction {
 
+	private static final long serialVersionUID = -7646520847139737802L;
+	
 	private String address = null;
-	private double hydrogen = 0;
+	private double hydrogen = 0.0;
+	private double max = 0.0;
 	
 	public HydrogenTransaction() {
 		super(TransactionType.HYDROGEN);
 	}
 	
-	public HydrogenTransaction(String address, double hydrogen) {
+	public HydrogenTransaction(String address, double hydrogen, double max) {
 		super(TransactionType.HYDROGEN);
+		
 		this.address = address;
 		this.hydrogen = hydrogen;
+		this.max = max;
 	}
 
 	@Override
