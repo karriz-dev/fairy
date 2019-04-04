c3cc1c97e488fc03f9c23b47127dac7c4a925d13
ldh1428a <ldh1428a@naver.com>
2019-04-04 10:36:42 +0900

    hydrogen_transaction_modify_commit

--- src/fairy/valueobject/managers/transaction/HydrogenTransaction.java
+++ src/fairy/valueobject/managers/transaction/HydrogenTransaction.java
@@ -6,7 +6,8 @@ public class HydrogenTransaction extends Transaction {
 
 	private static final long serialVersionUID = -7646520847139737802L;
 	
-	private String address = null;
+	private String toAddress = null;
+	private String fromAddress = null;
 	private double hydrogen = 0.0;
 	private double max = 0.0;
 	
@@ -14,31 +15,69 @@ public class HydrogenTransaction extends Transaction {
 		super(TransactionType.HYDROGEN);
 	}
 	
-	public HydrogenTransaction(String address, double hydrogen, double max) {
+	public HydrogenTransaction(String toAddress, String fromAddress, double hydrogen, double max) {
 		super(TransactionType.HYDROGEN);
-		
-		this.address = address;
+		this.toAddress = toAddress;
+		this.fromAddress = fromAddress;
 		this.hydrogen = hydrogen;
 		this.max = max;
 	}
 
+	public String getToAddress() {
+		return toAddress;
+	}
+
+	public String getFromAddress() {
+		return fromAddress;
+	}
+
+	public double getHydrogen() {
+		return hydrogen;
+	}
+
+	public double getMax() {
+		return max;
+	}
+
 	@Override
 	protected byte[] getDatasBytes() {
-		byte[] addressBytes = address.getBytes();
+		byte[] toAddressBytes = toAddress.getBytes();
+		byte[] fromAddressBytes = fromAddress.getBytes();
 		
-		byte[] result = new byte[addressBytes.length + 16];
+		byte[] result = new byte[toAddressBytes.length + fromAddressBytes.length + 16];
+
+		int offset = 0;
 		
-		System.arraycopy(addressBytes, 0, result, 0, addressBytes.length);
+		System.arraycopy(Convert.intToByteArray(toAddressBytes.length), 0, result, offset, 4);
 
-		System.arraycopy(Convert.doubleToBytes(this.hydrogen), 0, result, addressBytes.length, 8);
+		offset += 4;
+		
+		System.arraycopy(toAddressBytes, 0, result, offset, toAddressBytes.length);
 
-		System.arraycopy(Convert.doubleToBytes(this.max), 0, result, addressBytes.length + 8, 8);
+		offset += toAddressBytes.length;
+		
+		System.arraycopy(Convert.intToByteArray(fromAddressBytes.length), 0, result, offset, 4);
+		
+		offset += 4;
 
-		return null;
+		System.arraycopy(fromAddressBytes, 0, result, offset, fromAddressBytes.length);
+		
+		offset += fromAddressBytes.length;
+		
+		System.arraycopy(Convert.doubleToBytes(this.hydrogen), 0, result, offset, 8);
+		
+		offset += 8;
+
+		System.arraycopy(Convert.doubleToBytes(this.max), 0, result, offset, 8);
+		
+		offset += 8;
+
+		return result;
 	}
 
 	@Override
 	public String toString() {
-		return "HydrogenTransaction [address=" + address + ", hydrogen=" + hydrogen + "]";
+		return "HydrogenTransaction [toAddress=" + toAddress + ", fromAddress=" + fromAddress + ", hydrogen=" + hydrogen
+				+ ", max=" + max + "]";
 	}
 }
