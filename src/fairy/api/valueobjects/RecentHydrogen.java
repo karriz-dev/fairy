0178c3bcb3fa847449e6339a3286e6b96322033c
ldh1428a <ldh1428a@naver.com>
2019-04-18 23:53:18 +0900

    recent_hydrogen_commit

--- src/fairy/api/valueobjects/RecentHydrogen.java
+++ src/fairy/api/valueobjects/RecentHydrogen.java
@@ -4,18 +4,15 @@ public class RecentHydrogen implements Recent {
 	
 	private String from = null;
 	private String to = null;
-	
+	private long transactionTimestamp = 0L;
 	private double value = 0.0;
 	private double max = 0.0;
-	private long timestamp = 0L;
-	private long height = 0L;
-	
-	public RecentHydrogen(String from, String to, double value, double max, long timestamp, long height) {
+
+	public RecentHydrogen(String from, String to, long transactionTimestamp, double value, double max) {
 		this.from = from;
 		this.to = to;
+		this.transactionTimestamp = transactionTimestamp;
 		this.value = value;
 		this.max = max;
-		this.timestamp = timestamp;
-		this.height = height;
 	}
 }
