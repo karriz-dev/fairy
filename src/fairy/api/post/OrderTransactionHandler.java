10b39615dc6fe8701ff95e51508264a87a5536c8
ldh1428a <ldh1428a@naver.com>
2019-04-18 23:55:46 +0900

    order_transaction_handler_add_commit

---
+++ src/fairy/api/post/OrderTransactionHandler.java
@@ -0,0 +1,78 @@
+package fairy.api.post;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.security.KeyPair;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import fairy.api.Handler;
+import fairy.core.managers.key.KeyManager;
+import fairy.core.managers.transaction.TransactionManager;
+import fairy.core.net.communicator.Linker;
+import fairy.valueobject.managers.transaction.OrderTransaction;
+import fairy.valueobject.managers.transaction.Transaction;
+
+public class OrderTransactionHandler extends Handler implements HttpHandler {
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		if(exchange.getRequestMethod().toUpperCase().equals(Handler.POST))
+		{
+			exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+			exchange.getResponseHeaders().set("Access-Control-Max-Age", "3600");
+			exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "x-requested-with");
+			exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+
+			Map<String, Object> parameters = new HashMap<String, Object>();
+	        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+	        BufferedReader br = new BufferedReader(isr);
+	        String query = br.readLine();
+	        parseQuery(query, parameters);
+	        
+	        String response = "";
+
+	        if(!parameters.isEmpty())
+	        {
+	            String address = (String)parameters.get("address");
+	        	short type = (short)Integer.parseInt((String)parameters.get("type"),16);
+	        	long maxtime = Long.parseLong((String)parameters.get("maxtime"));
+	        	
+	        	Transaction tx = new OrderTransaction(address, type, maxtime);
+
+	            KeyPair pair = KeyManager.getInstance().Get().getPair();
+
+	            tx.setSignature(TransactionManager.getInstance().Sign(tx.getBytes(), pair.getPrivate()));
+	            tx.setPublicKey(pair.getPublic());
+	            
+	            if(Linker.getInstance().broadcastingTransactrionUsingSerialization(tx))
+	            {
+	            	response = "Transaction was broadcasted !! \r\n" + tx.toString();
+	            }else{
+	            	response = "Transaction wasn't broadcasted !! \r\n";
+	            }
+	            
+	            exchange.sendResponseHeaders(200, response.length());
+	        }
+	        else
+	        {
+	        	exchange.sendResponseHeaders(400, 0);
+	        	exchange.close();
+	        }
+	        
+	        OutputStream os = exchange.getResponseBody();
+	        
+	        os.write(response.toString().getBytes());
+	        
+	        os.close();
+		}
+		else {
+			exchange.sendResponseHeaders(400, 0);
+			exchange.close();
+		}
+	}
+}
