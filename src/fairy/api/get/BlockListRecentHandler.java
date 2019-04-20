4a1e927000c4462d3984c5bcb64e2f6b26ab254b
ldh1428a <ldh1428a@naver.com>
2019-04-20 17:02:47 +0900

    blocklist_recent_handler_add_commit

---
+++ src/fairy/api/get/BlockListRecentHandler.java
@@ -0,0 +1,48 @@
+package fairy.api.get;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import fairy.api.Handler;
+import fairy.core.managers.ledger.LedgerManager;
+
+public class BlockListRecentHandler extends Handler implements HttpHandler {
+	
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		if(exchange.getRequestMethod().toUpperCase().equals(Handler.GET))
+		{
+			exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+			exchange.getResponseHeaders().set("Access-Control-Max-Age", "3600");
+			exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "x-requested-with");
+			exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+
+			Gson gson = new Gson();
+		
+			String response = gson.toJson(LedgerManager.getInstance().getLatestBlock());
+			
+		    OutputStream os = exchange.getResponseBody();
+		    
+		    if(response != "")
+		    {
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }else
+		    {
+		    	response = "block is not load wating for create block..";
+		    	exchange.sendResponseHeaders(200, response.length());
+		    	os.write(response.toString().getBytes());
+		    }
+		    
+		    os.close();
+		}
+		else {
+			exchange.sendResponseHeaders(400, 0);
+			exchange.close();
+		}
+	}
+}
