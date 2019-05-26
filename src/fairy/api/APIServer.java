ae3a487acb398d615fee2f0b1309ea7a2c6817ec
ldh1428a <ldh1428a@naver.com>
2019-05-26 22:40:29 +0900

    api_modify_commit

--- src/fairy/api/APIServer.java
+++ src/fairy/api/APIServer.java
@@ -7,10 +7,14 @@ import com.sun.net.httpserver.HttpServer;
 import fairy.api.get.BlockListHandler;
 import fairy.api.get.BlockListRecentHandler;
 import fairy.api.get.LastestBlockHeightHandler;
+import fairy.api.get.SearchHandler;
 import fairy.api.get.TokenBalanceHandler;
+import fairy.api.get.TransactionCountHandler;
+import fairy.api.get.TransactionListDetailHandler;
 import fairy.api.get.TransactionListHandler;
 import fairy.api.get.TransactionSearchHandler;
 import fairy.api.get.TransactionSearchRecentHandler;
+import fairy.api.get.TransactionSellHydrogenBalanceHandler;
 import fairy.api.post.DeliveryTransactionHandler;
 import fairy.api.post.HydrogenTransactionHandler;
 import fairy.api.post.OrderTransactionHandler;
@@ -36,11 +40,13 @@ public class APIServer {
 			httpServer.createContext("/transaction/order",new OrderTransactionHandler());
 			httpServer.createContext("/transaction/sell", new SellHydrogenTransactionHandler());
 			
-			
 			httpServer.createContext("/transaction/list",new TransactionListHandler());
+			httpServer.createContext("/transaction/list/detail",new TransactionListDetailHandler());
+			
 			httpServer.createContext("/transaction/search", new TransactionSearchHandler());
 			httpServer.createContext("/transaction/search/recent", new TransactionSearchRecentHandler());
 			
+			httpServer.createContext("/transaction/count", new TransactionCountHandler());
 			
 			//block
 			httpServer.createContext("/block/list",new BlockListHandler());
@@ -53,10 +59,16 @@ public class APIServer {
 			//hydrogen
 			httpServer.createContext("/hydrogen/balance",new TokenBalanceHandler());
 			
+			//sellhydrogen
+			httpServer.createContext("/sell/hydrogen/balance",new TransactionSellHydrogenBalanceHandler());
+			
+			//search
+			httpServer.createContext("/search", new SearchHandler());
+			
 			httpServer.setExecutor(null);
 			httpServer.start();
 			
-			Debugger.Log(this, "API server(" + 12075 + ") is started !!");
+			Debugger.Log(this, "API server(" + 5000 + ") is started !!");
 		}catch(Exception e) {
 			
 		}
